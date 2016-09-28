e3b56319164a1617fa39b98152f52a96fc46c460
Tobias <tobias@sernhede.com>
2016-09-28 14:32:59 +0200

    Updated the layout

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tobiassernhede.github.io
-My personal github site
+My personal github site.
+Built with:
+
+Bootstrap 3 grid
+jQuery
+Sass
+React
+and some love
+
+## License
+
+MIT License
+
+Social icon font from http://drinchev.github.io/monosocialiconsfont/
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,36 @@
 jQuery(function($) {
+	var didScroll;
 
+		var lastScrollTop = 0;
+		var delta = 25;
+		var headerHeight = $('#masthead').outerHeight();
+		$(window).scroll(function(){
+		    didScroll = true;
+		});
+
+		setInterval(function() {
+		    if (didScroll) {
+		        hasScrolled();
+		        didScroll = false;
+		    }
+		}, 150);
+
+		function hasScrolled() {
+		    var st = $(this).scrollTop();
+
+		    // Make sure they scroll more than delta
+		    if(Math.abs(lastScrollTop - st) <= delta) {
+		        return;
+		    }
+
+		    // If they scrolled down and are past the navbar, add class .nav-up.
+		    // This is necessary so you never see what is "behind" the navbar.
+		    if(st < headerHeight + 250) {
+		    	$('body').removeClass('nav-not-top').addClass('nav-top');
+		    } else {
+		    	$('body').removeClass('nav-top').addClass('nav-not-top');
+		    }
+
+		    lastScrollTop = st;
+		}
 });
